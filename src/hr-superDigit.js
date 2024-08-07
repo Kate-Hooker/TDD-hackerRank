545f718c6a78a8a8568280282f9a05eff6a4a53e
Kate-Hooker <katehooker88@gmail.com>
2024-08-07 21:51:37 +1200

    tests not working

--- src/hr-superDigit.js
+++ src/hr-superDigit.js
@@ -1,14 +1,22 @@
 
+
 function superDigit(n, k) {
   // Write your code here
-  let numberToBreakdown = n.split('')
+   
+    let numbersToBreakdown = n.split('').map(Number) 
+
+    return numbersToBreakdown
+  // returns ['a', 'b', 'c']
 
 
 
-  return
+
 }
 
 //string n: a string representation of an integer
 //int k: the times to concatenate  to make 
+superDigit()
+// split string into an array of numbers, add, return, make string
+// use a while loops based on K 
 
 export default superDigit
